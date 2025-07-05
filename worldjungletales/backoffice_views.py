5677964c19ff98a59c092dc1f40ad5873a27574a
WawinyEdwin <wawinyedwin44@gmail.com>
2025-07-05 15:40:49 +0300

    fix: show all draft articles

--- worldjungletales/backoffice_views.py
+++ worldjungletales/backoffice_views.py
@@ -190,7 +190,7 @@ def drafts(request):
 @user_passes_test(lambda u: u.is_superuser)
 def draft_publish(request, article_pk):
     author = request.user
-    articles = Article.objects.filter(author=author, status=0)
+    articles = Article.objects.filter(status=0)
     Article.objects.filter(author=author, pk=article_pk).update(status=1)
     topics = Topic.objects.filter(status=1)
     context = {}
